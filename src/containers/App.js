457ed4f1987fdafded696f2b469ab3e422b54254
Louis Gourain <louis.gourain@gmail.com>
2019-02-13 22:59:08 +0100

    adding the connect function

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
 class App extends Component {
 	constructor() {
 		super();
 		this.state  = {
-			robots: [],
-			searchfield: ''			
+			robots: []		
 		}
 	}
 
@@ -19,29 +33,24 @@ class App extends Component {
 			.then(users => this.setState({ robots: users }));
 	}
  
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value });
-	}
-
 	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { robots } = this.state;
+		const { searchField, onSearchChange } = this.props;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		});
-		if(this.state.robots.length === 0) {
-			return <h1>Loading</h1>
-		}
-		else {
-			return (
+		return !robots.length ?
+			<h1>Loading</h1> :
+			(
 				<div className='tc'>
 					<h1 className='f2'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange} />
+					<SearchBox searchChange={onSearchChange} />
 					<Scroll>
 						<CardList robots={filteredRobots} />
 					</Scroll>
-				*</div>
-			);					
-		}
+				</div>
+			);
 	}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
